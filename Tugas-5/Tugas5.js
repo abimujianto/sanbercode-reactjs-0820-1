2dbce5918f982012a66e38ec930be97b18d2183a
abimujianto <abimujianto1997@gmail.com>
2020-08-29 11:00:28 +0800

    revisi Tugas 5

--- Tugas-5/Tugas5.js
+++ Tugas-5/Tugas5.js
@@ -52,25 +52,25 @@ var arrObj = [
   {
     nama: "strawberry",
     warna: "merah",
-    ada_bijinya: "tidak",
+    ada_bijinya: false,
     harga: 9000,
   },
   {
     nama: "jeruk",
     warna: "oranye",
-    ada_bijinya: "ada",
+    ada_bijinya: true,
     harga: 8000,
   },
   {
     nama: "Semangka",
     warna: "Hijau & Merah",
-    ada_bijinya: "ada",
+    ada_bijinya: true,
     harga: 10000,
   },
   {
     nama: "Pisang",
     warna: "Kuning",
-    ada_bijinya: "tidak",
+    ada_bijinya: false,
     harga: 5000,
   },
 ];
